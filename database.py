3037aad3e76cbcf28af381649dbb97ee600ecca4
denysolshanskiy-hue <denysolshanskiy@gmail.com>
2026-01-22 15:19:43 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,3 @@
-import sqlite3
-
-DB_PATH = "mafia_bot.db"
-
-def get_connection():
-    return sqlite3.connect(DB_PATH)
-
-
 def init_db():
     conn = get_connection()
     cursor = conn.cursor()
@@ -47,3 +39,4 @@ def init_db():
 
     conn.commit()
     conn.close()
+
